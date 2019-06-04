a7275457eeaed4d76edd2d4880f3ec93f679775c
ykhirose <hirose@leomo.io>
2019-06-04 16:42:18 +0900

    First commit

---
+++ init.el
@@ -0,0 +1,13 @@
+;; function for adding load-path
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(format (concat "path =" path))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+;; Add directories and sub directories to load-path
+(add-to-load-path "elisp" "conf" "public_repos")
