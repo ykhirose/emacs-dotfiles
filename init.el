ecb24f491a0553cb2d4bd031bcf559d865cde113
ykhirose <hirose.work27@gmail.com>
2019-06-05 19:30:39 +0900

    install some packages

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; function for adding load-path
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
@@ -11,3 +18,120 @@
 
 ;; Add directories and sub directories to load-path
 (add-to-load-path "elisp" "conf" "public_repos")
+
+;; MELPA
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+;;                         ("melpa" . "https://melpa.org/packages/")))
+
+;; MELPA-stable
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t) 
+
+;; Marmalade
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; Org
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; initialize
+(package-initialize)
+
+;; set default-directory
+(setq default-directory "C:\\Users\\hiros\\")
+
+;; theme
+(setq custom-theme-directory "~/.emacs.d/themes/")
+(load-theme 'atom-one-dark t)
+
+;; display line-number
+(global-linum-mode t)
+
+;; display line-number to mode-line
+(line-number-mode t)
+
+;; display column to mode-line
+(column-number-mode t)
+
+;; display file path to title bar
+(setq frame-title-format "%f")
+
+;; TAB count
+(setq-default tab-width 2)
+
+;; Highlight current line
+(defface my-hl-line-face
+	'((((class color) (background dark))
+		 (:background "DarkSlateGrey" t))
+		(((class color) (background light))
+		 (:background "LightGoldenrodYellow" t))
+		(t (:bold t)))
+	"hl-line's my face")
+(setq hl-line-face 'myp-hl-line-face)
+(global-hl-line-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auto-complete helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
+
+;;
+;; Auto Complete
+;;
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
